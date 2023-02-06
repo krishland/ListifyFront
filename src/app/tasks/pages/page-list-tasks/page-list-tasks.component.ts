a767a73e0baa12478f576802b63e01c5ea70197e
Pierre-Etienne <pierre-etienne@MacBook-Pro-de-Pierre-Etienne.local>
2023-02-06 14:16:14 +0100

    css list

--- src/app/tasks/pages/page-list-tasks/page-list-tasks.component.ts
+++ src/app/tasks/pages/page-list-tasks/page-list-tasks.component.ts
@@ -7,32 +7,31 @@ import { TasksService } from '../../services/tasks.service';
 @Component({
   selector: 'app-page-list-tasks',
   templateUrl: './page-list-tasks.component.html',
-  styleUrls: ['./page-list-tasks.component.scss']
+  styleUrls: ['./page-list-tasks.component.scss'],
 })
 export class PageListTasksComponent implements OnInit {
-
   public collection!: Task[];
 
   public states = Object.values(State);
 
   constructor(private tasksService: TasksService, private router: Router) {
-    this.tasksService.collection.subscribe(data => {
+    this.tasksService.collection.subscribe((data) => {
       this.collection = data;
+      console.log(data);
     });
-   }
-
-  ngOnInit(): void {
   }
 
-  public changeState(task: Task, event: Event){
+  ngOnInit(): void {}
+
+  public changeState(task: Task, event: Event) {
     const target = event.target as HTMLSelectElement;
     const state = target.value as State;
-    this.tasksService.changeState(task, state).subscribe(data => {
+    this.tasksService.changeState(task, state).subscribe((data) => {
       Object.assign(task, data);
-    })
+    });
   }
 
-  public goToEdit(task: Task){
-    this.router.navigate(["edit", task.id])
+  public goToEdit(task: Task) {
+    this.router.navigate(['edit', task.id]);
   }
 }
